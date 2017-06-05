008d0c83e1f160ea9ebc109a611bc0b2bb5bb0d7
Aryan Gupta <kuantum.freak@gmail.com>
2017-06-04 21:09:45 -0400

    Add Files

---
+++ Paddle.cpp
@@ -0,0 +1,20 @@
+/**    
+***    Copyright (c) 2017 The Gupta Empire - All Rights Reserved
+***    Unauthorized copying of this file, via any medium is strictly prohibited
+***    Proprietary and confidential
+***    
+***    Written by Aryan Gupta <me@theguptaempire.net>
+***    
+***=============================================================================
+***    @author          Aryan Gupta
+***    @project         
+***    @title           main
+***    @date            (YYYY-MM-DD)
+***    @version         
+***    @brief           Contains program entry point and misc. functions
+***=============================================================================
+**/
+
+#include "info.h"
+
+#include "main.h"
---
+++ Paddle.h
@@ -0,0 +1,18 @@
+/**    
+***    Copyright (c) 2017 The Gupta Empire - All Rights Reserved
+***    Unauthorized copying of this file, via any medium is strictly prohibited
+***    Proprietary and confidential
+***    
+***    Written by Aryan Gupta <me@theguptaempire.net>
+***    
+***=============================================================================
+***    @author          Aryan Gupta
+***    @project         
+***    @title           main
+***    @date            (YYYY-MM-DD)
+***    @version         
+***    @brief           Contains program entry point and misc. functions
+***=============================================================================
+**/
+
+#pragma once
